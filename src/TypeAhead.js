1e9989072a524c4f42714f6886dc1ed278a66594
Nathaniel Angelico Tabanao <nathanielangelico23@gmail.com>
2021-02-21 20:41:34 +0800

    Fixing API Multiiple calls

---
+++ src/TypeAhead.js
@@ -0,0 +1,80 @@
+import './TypeAhead.css'
+import { useState, useEffect } from 'react'
+
+function TypeAhead() {
+    const [state, setState] = useState({
+      input: '',
+      isShow: true,
+      user: {
+        avatar_url: '',
+        login: ''
+      },
+      error: false
+    })
+  
+  
+    // useEffect(() => {
+    //   fetch(`https://api.github.com/users/${state.input}`)
+    //     .then( response => response.json())
+    //     .then( data => { 
+    //         const { avatar_url, login } = data
+    //         setState({ 
+    //             ...state,
+    //             user: { avatar_url, login },
+    //             error: false
+    //         })
+    //         .console.log(avatar_url)
+    //     })
+    //     .catch(e => {
+    //         setState({...state,error: true})
+    //         console.log('error', e.message)
+    //     })
+    // }, [state.input])
+  
+  
+    const inputChange = (e) => {
+      setState({...state,
+        input: e.target.value})
+      //const { id, value } = e.target
+      //setState({ ...state, [id]: value, isShow: true })
+      console.log('state: input', state.input, 'e.target.value', e.target.value)
+    }
+  
+    const selectOption = (user) => {
+      const { download_url, name } = user
+      setState({ ...state, input: name, isShow: false, image: download_url })
+    }
+  
+  
+    return (
+        <div className="typeahead">
+            <input 
+              type="text" 
+              id="input" 
+              //value={state.input} 
+              onChange={inputChange} 
+              />
+            
+            <div className="typeahead-content">
+                {
+                    (state.input && state.isShow) && (
+                    <div
+                        onClick={() => selectOption(state.user.login)} 
+                        className="typeahead-content-list"
+                      >
+                        <img 
+                          className="typeahead-content-list-image"  
+                          src={state.user.avatar_url} alt={`${state.user.avatar_url}`} 
+                          />
+                        <span className="typeahead-content-list-name">{state.user.login}</span>
+                    </div>
+                    )
+                }
+            </div>
+        </div>
+        
+        )
+    }
+    
+    
+export default TypeAhead;
